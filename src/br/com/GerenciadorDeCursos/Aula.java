defbc3586e7fc7d51ebce182d0c84a6f43ecc7cd
AurelioNM <aureliomororo300@gmail.com>
2021-11-08 09:36:42 -0300

    Aplicando ArrayList em Classes

---
+++ src/br/com/GerenciadorDeCursos/Aula.java
@@ -0,0 +1,30 @@
+package br.com.GerenciadorDeCursos;
+
+public class Aula implements Comparable<Aula> {
+
+    private String titulo;
+    private int tempo;
+
+    public Aula(String titulo, int tempo) {
+        this.titulo = titulo;
+        this.tempo = tempo;
+    }
+
+    public int getTempo() {
+        return tempo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    @Override
+    public String toString() {
+        return "[Aula:  " + "titulo='" + titulo + '\'' + ", tempo=" + tempo + ']' + System.lineSeparator();
+    }
+
+    @Override
+    public int compareTo(Aula outraAula) {
+        return this.titulo.compareTo(outraAula.titulo);
+    }
+}
